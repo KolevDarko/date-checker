d82c3832ed14d267dd033ce3994921db203acb73
darko <kolevdarko@gmail.com>
2020-01-17 17:13:42 +0100

    store test and get_by_id method

--- api/models.py
+++ api/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
-class Store(models.Model):
+class ModelMixin():
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.objects.get(pk=id)
+
+class Store(models.Model, ModelMixin):
     location = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
 
-class Product(models.Model):
+class Product(models.Model, ModelMixin):
     name = models.CharField(max_length=300)
     price = models.FloatField()
 
-class ProductInStore(models.Model):
+class ProductInStore(models.Model, ModelMixin):
 
     store_id = models.ForeignKey(Store, on_delete=models.DO_NOTHING, related_name='product_items')
     product_id = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name='products_in_store')
--- api/tests.py
+++ api/tests.py
@@ -15,3 +15,22 @@ class ProductTests(APITestCase):
         self.assertEqual(Product.objects.count(), 1)
         self.assertEqual(Product.objects.get().name, 'Coca Cola Cherry')
         self.assertEqual(Product.objects.get().price, 30)
+
+    def test_get_product(self):
+        product = Product.objects.create(name='paracetamol', price=12)
+        product_url = reverse('product-detail', args=[product.id])
+        response = self.client.get(product_url)
+        self.assertEqual(response.data['name'], 'paracetamol')
+        self.assertEqual(response.data['price'], 12)
+
+class StoreTest(APITestCase):
+
+    def test_create_store(self):
+        url = reverse('store-list')
+        location = 'Skopska 13'
+        data = {'location': location, 'address': 'skopje'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        store_url = response.data['url']
+        get_response = self.client.get(store_url)
+        self.assertEqual(get_response.data['location'], location)
