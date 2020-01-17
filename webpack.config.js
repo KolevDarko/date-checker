a828d63207b41515da632bec9d8cece35512017a
darko <kolevdarko@gmail.com>
2020-01-17 18:50:15 +0100

    testing frontend stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 const path = require("path");
 var BundleTracker = require('webpack-bundle-tracker');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   context: __dirname,
 
   entry: {
-    bundle: "./frontend/src/index.js"
+    bundle: "./public/javascripts/index.js"
   },
-
+  mode: 'development',
   output: {
-    filename: "[name].js",
-    path: path.resolve("./frontend/bundles")
+    filename: "bundle.js",
+    path: path.resolve("./public/bundles")
   },
 
   devtool: "source-map",
 
   plugins: [
-    new BundleTracker({filename: './webpack-stats.json'})
+    new BundleTracker({filename: './webpack-stats.json'}),
+    new MiniCssExtractPlugin({
+      filename: 'css/mystyles.css'
+    }),
   ],
 
   module: {
@@ -29,7 +33,21 @@ module.exports = {
         use: [
           { loader: "babel-loader" }
         ]
-      }
-    ]
+      },
+      {
+      test: /\.scss$/,
+      use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+    }]
   }
 };
