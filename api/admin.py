4574211a6d778de272ed5e72126823951f95a564
darko <kolevdarko@gmail.com>
2020-01-21 18:17:22 +0100

    added requirements and admin working

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ProductInStore, Store, Product
+
+admin.site.register(Store)
+admin.site.register(Product)
+admin.site.register(ProductInStore)
--- datecheck/settings.py
+++ datecheck/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'datecheck.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
     }
 }
 
