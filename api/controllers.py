461e5c4d882d6ed714cbd20ffa9b59d8dd253aaa
darko <kolevdarko@gmail.com>
2020-07-14 18:17:53 +0200

    catching non existing product batch

--- api/controllers.py
+++ api/controllers.py
@@ -5,12 +5,15 @@ class BatchController:
 
     @classmethod
     def update_batch(cls, batch_id, new_quantity):
-        batch = ProductBatch.get_by_id(batch_id)
-        batch.update_quantity(new_quantity)
-        if new_quantity == 0:
-            batch_archive = ProductBatchArchive.create_batch_archive(batch)
-            BatchWarning.archive_warnings(batch.id, batch_archive.id)
-            batch.delete()
+        try:
+            batch = ProductBatch.get_by_id(batch_id)
+            batch.update_quantity(new_quantity)
+            if new_quantity == 0:
+                batch_archive = ProductBatchArchive.create_batch_archive(batch)
+                BatchWarning.archive_warnings(batch.id, batch_archive.id)
+                batch.delete()
+        except ProductBatch.DoesNotExist:
+            pass
 
     @classmethod
     def update_warnings(cls, batch_id, new_quantity):
